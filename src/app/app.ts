6a66acf11f1fc39848e82c8b370c5d831ffcdf8e
Kurt Joshua Cayaga <kurtcayagatash@gmail.com>
2025-09-09 21:51:18 +0800

    style: organize styles with tailwind

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './pages/header/header';
+import { Overview } from "./pages/overview/overview";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Overview],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
